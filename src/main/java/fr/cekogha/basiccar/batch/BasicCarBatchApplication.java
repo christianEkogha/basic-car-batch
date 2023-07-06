c862e1fea4e8fddcb73054db25c66f12ff2f5f10
christianEkogha <vekogha@gmail.com>
2023-07-06 10:56:15 +0200

    Update BasicCarBatchApplication.java
    management of dependency exclusions an imports

--- src/main/java/fr/cekogha/basiccar/batch/BasicCarBatchApplication.java
+++ src/main/java/fr/cekogha/basiccar/batch/BasicCarBatchApplication.java
@@ -10,10 +10,14 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
 @ComponentScan(basePackages = "fr.cekogha.basiccar.batch.*")
 public class BasicCarBatchApplication implements CommandLineRunner {
 
