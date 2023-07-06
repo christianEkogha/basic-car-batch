8fcf3b589ea156ff0304ee2fa47e8f2de3da3453
christianEkogha <vekogha@gmail.com>
2023-07-06 10:42:51 +0200

    Create Car.java

---
+++ src/main/java/fr/cekogha/basiccar/batch/entity/Car.java
@@ -0,0 +1,26 @@
+package fr.cekogha.basiccar.batch.entity;
+
+import com.datastax.driver.core.utils.UUIDs;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.cassandra.core.mapping.PrimaryKey;
+import org.springframework.data.cassandra.core.mapping.Table;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@Table
+public class Car {
+    @PrimaryKey
+    private UUID id = UUIDs.timeBased();
+    private String brand;
+    private String model;
+    private BigDecimal milestone = new BigDecimal(0);
+    private BigDecimal price = new BigDecimal(0);
+    private String year;
+    private Map<LocalDateTime, LocalDateTime> activity;
+}
